7092c9fba4866d8d9607511508154e321ef81275
Rikstone <tarikbouari@gmail.com>
2022-12-08 10:28:09 +0000

    Create foreign key on tables

--- db/migrate/20221208085911_create_likes.rb
+++ db/migrate/20221208085911_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :author_id
-      t.integer :post_id
+      t.references :users, null: false, foreign_key: true
+      t.references :posts, null: false, foreign_key: true
 
       t.timestamps
     end
