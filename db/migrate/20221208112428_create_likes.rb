0e247796ad8d8e388b6bf56bd6441889db6dbbb4
Rikstone <tarikbouari@gmail.com>
2022-12-08 13:08:36 +0000

    Migrate database

--- db/migrate/20221208112428_create_likes.rb
+++ db/migrate/20221208112428_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.references :users, null: false, foreign_key: true
-      t.references :posts, null: false, foreign_key: true
+      # t.bigint :author_id
+      # t.bigint :post_id
 
       t.timestamps
     end
--- db/migrate/20221208112819_create_comments.rb
+++ db/migrate/20221208112819_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.references :users, null: false, foreign_key: true
-      t.references :posts, null: false, foreign_key: true
+      # t.bigint :author_id
+      # t.bigint :post_id
       t.text :text
 
       t.timestamps
