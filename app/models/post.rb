e2002943978f977099446b38b5beb06af54fb2c8
Rikstone <tarikbouari@gmail.com>
2022-12-09 10:51:35 +0000

    Add recent comment method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,17 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    has_many :likes
-    has_many :comments
+  belongs_to :users
+  has_many :likes
+  has_many :comments
+
+  after_save :update_post_counter
+
+  private
+
+  def update_post_counter
+    posts.increment!(:posts_counter)
+  end
+
+  def rencent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
