d7ba67a6c65349569b2a49d736d28b3dade5183e
Rikstone <tarikbouari@gmail.com>
2022-12-09 10:53:07 +0000

    Create method for recent post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :likes
-    has_many :comments
+  has_many :posts
+  has_many :likes
+  has_many :comments
+
+  private
+
+  def most_recent_post
+    post.order(created_at: :desc).limit(3)
+  end
 end
