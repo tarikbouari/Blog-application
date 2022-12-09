30a73b22d28ac0a8a31cba108cfa3f9718cb1ffb
Rikstone <tarikbouari@gmail.com>
2022-12-09 10:49:57 +0000

    Create recent comment method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
+  belongs_to :users
+  belongs_to :posts
+
+  def comments_counter
+    comments.increment!(:comments_counter)
+  end
 end
