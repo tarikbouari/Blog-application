4bf6fbc168ef93d4d1e437b4f4ae53685e78df11
Rikstone <tarikbouari@gmail.com>
2022-12-13 12:22:42 +0000

    Update action show users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
  
+  #setting the home page
+  root "users#index"
+  
   # Display all the list of users
   get 'users/', to: 'users#index' 
 
@@ -11,8 +14,8 @@ Rails.application.routes.draw do
   get 'users/:id', to: 'users#show'
 
   #display all the post for a specific user
-  get 'users/:user_id/posts', to: 'posts#index
-
+  get 'users/:user_id/posts', to: 'posts#index'
+  
   #display a user specific post 
   get 'users/:user_id/posts/:id', to: 'posts#show'
 
