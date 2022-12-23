2ba15280ad3bb1ceb9fe2022a59cb931a911c2ce
Rikstone <tarikbouari@gmail.com>
2022-12-23 19:42:42 +0000

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,13 +9,12 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  private
 
-  def update_post_counter
+  def posts_counter
     user.increment!(:posts_counter)
   end
 
-  def rencent_comments
-    post.order(created_at: :desc).limit(5)
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
   end
 end
